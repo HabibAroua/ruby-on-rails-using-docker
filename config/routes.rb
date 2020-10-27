cf1a03f7bc32516f77553fa3c924c10caa1bae2b
HabibAroua <habibha.aroua82@gmail.com>
2020-10-27 23:42:09 +0100

    add auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+    root 'home#index'
+    get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     #get 'test/welcome' to: 'TestController#welcome'
     get '/test/welcome'
-    get '/' , to: 'test#main'
+    get '/hello' , to: 'test#main'
     post 'test/post' , to: 'test#post'
+    #https://medium.com/@wintermeyer/authentication-from-scratch-with-rails-5-2-92d8676f6836
     
 end
